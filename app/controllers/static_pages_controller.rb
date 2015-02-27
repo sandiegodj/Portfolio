0bf94843e4d92edd43dfe3b818a3a914abde9e97
sandiegodj <davidjanczyn@gmail.com>
2015-02-27 11:41:31 -0800

    update quote query

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @quote = Quote.find(1 + rand(Quote.count))
+    @quote = Quote.find(1 + (rand(Quote.count).round)
     @contact = Contact.new
   end
 end
