df1c9a70637c20f9e5779fbd68332b6a9cc77162
sandiegodj <davidjanczyn@gmail.com>
2015-03-03 19:46:37 -0800

    add DB save for messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,6 +3,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
+      Message.create(email: params[:contact][:email], name: params[:contact][:email], message: params[:contact][:message])
       flash.now[:notice] = "Thanks for the message.  David will get back to you shortly"
       redirect_to root_path
     else
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131230807) do
+ActiveRecord::Schema.define(version: 20150304034436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.text     "quote"
